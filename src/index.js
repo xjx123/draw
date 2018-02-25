040f7dfabd757a5e83a5780e8e3648f363dd7a04
xjx <644233452@qq.com>
2018-02-25 22:31:48 +0800

    add

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import Vue from 'vue';
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'hello vue416'
+        message: 'hello vue12'
     }
 });
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
@@ -7,23 +8,22 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: 'index.html',
-            inject: true
-        })
+            filename: "index.html", // 将HTML写入到指定的文件
+            template: 'index.html', // 模板html, 一开始加载的html
+            inject: true    // true: 代表所有的js资源将放置在body元素底部
+        }),
+        new webpack.HotModuleReplacementPlugin()    // 热替换
     ],
+    // 解释在这 https://github.com/vuejs-templates/webpack/issues/215 
     resolve: {
         alias: {
             vue: 'vue/dist/vue.js'
         }
     },
-    devServer: {
-        contentBase: './dist'
-    },
-    devtool: 'inline-source-map',
+    devtool: 'inline-source-map', // 调试方便，可以看到错误信息出现在未生成之前的js中
     output: {
         filename: '[name].js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        publicPath: '/' // 确保文件资源能够正确的访问
     }
 };
